61d645dc5219b225cc48cf11abb1ef6adaf5bbe8
Nicolas Butera <126295707+niButera@users.noreply.github.com>
2024-04-24 22:19:15 -0300

    corrections
    navbar, popups

--- js/cadastro.js
+++ js/cadastro.js
@@ -23,7 +23,7 @@ document.addEventListener("submit", (event) => {
             alert("A senha não preenche os requisitos.")
         }
         else if(samePassword == false){
-            alert("A senha não corresponde com \"confirmar senha\".")
+            alert("A senha não corresponde com \"a senha anterior\".")
         }
 });
 
@@ -49,15 +49,15 @@ document.addEventListener("DOMContentLoaded", () => {
     senhaInput.addEventListener("focusout", () => {
         var frase = "";
         if (senhaInput.value.length < 7){
-            frase = "Faltou preencher a quantidade de Caracteres, Restantes: "+(7-senhaInput.value.length)+".\n"
+            frase = "Quantidade mínima de caracteres não correspondida , Mínimo necessário: "+(7-senhaInput.value.length)+".\n"
         }
         const isContainsNumber = /^(?=.*[0-9]).*$/;
         if (!isContainsNumber.test(senhaInput.value)){
-            frase += "Faltou algum caracter númerico.\n"
+            frase += "Necessário algum caracter númerico.\n"
         }
         const isContainsSymbol = /^(?=.*[~`!@#$%^&*()--+={}\[\]|\\:;"'<>,.?/_₹]).*$/;
         if (!isContainsSymbol.test(senhaInput.value)){
-            frase += "Faltou algum caracter especial."
+            frase += "Necessário algum caracter especial."
         }
         if (frase.length > 0){
             alert(frase);
